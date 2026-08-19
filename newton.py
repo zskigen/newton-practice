21c8b44e31aa34a90d7b7837d4c9c1a28c037f9c
zskigen <z_skigen@berkeley.edu>
2026-08-19 14:07:24 -0700

    formatting fixes in ruff

--- newton.py
+++ newton.py
@@ -1,36 +1,33 @@
 def derivative(f, x, h=1e-5):
-        """
-        compute derivative
-        """
-        return (f(x + h) - f(x - h)) / (2 * h)
+    """
+    compute derivative
+    """
+    return (f(x + h) - f(x - h)) / (2 * h)
+
 
 def function_value(function, value):
+    """
+    compute function at a value
+    """
     return function(value)
 
 
-def newtons_method(x0, function): 
-     while True:
-         #calculate function value and derivative value at current x
-         func_val = function_value(function, x0) 
-         derivative_calc = derivative(function, x0)
+def newtons_method(x0, function):
+    """
+    calculate function value and derivative value at current x, check if derivative is close to 0
+    then calculate the next x, and check if the difference between the new x and old x is small, and
+    then update the old x with the new x and repeat
+    """
+    while True:
+        func_val = function_value(function, x0)
+        derivative_calc = derivative(function, x0)
 
-         #check if derivative is too close to 0
-         if abs(derivative_calc) < 1e-9:
-             print("too close to 0")
-             break
+        if abs(derivative_calc) < 1e-9:
+            print("too close to 0")
+            break
 
-         #Calculate the next x
-         x_n1 = x0 - (func_val / derivative_calc)
+        x_n1 = x0 - (func_val / derivative_calc)
 
-        #check if the difference between x_n1 and x0 is tiny
-         if abs(x_n1 - x0) < 1e-9:
-             return x_n1
-       
-        #update  current x to be the new x_new for the next round
+        if abs(x_n1 - x0) < 1e-9:
+            return x_n1
         x0 = x_n1
-
-
-    
-    
-    
-    
