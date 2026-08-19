2bec9a838caa8cf1b61942601f3900bd4fcfe9ae
zskigen <z_skigen@berkeley.edu>
2026-08-19 12:15:12 -0700

    corrected code

--- newton.py
+++ newton.py
@@ -6,32 +6,31 @@ def derivative(f, x, h=1e-5):
 
 def function_value(function, value):
     return function(value)
-    
+
 
 def newtons_method(x0, function): 
      while True:
          #calculate function value and derivative value at current x
-         func_val = function_value(value) 
-         derivative_calc = derivative(x0)
+         func_val = function_value(function, x0) 
+         derivative_calc = derivative(function, x0)
 
-         #check if derivative is too close to 0 
+         #check if derivative is too close to 0
+         if abs(derivative_calc) < 1e-9:
+             print("too close to 0")
+             break
 
-         if derivative_calc < 1e-9:
-             then print("too close to 0")
-
-        #Calculate the next x
-
-        x_n1 = current_x - f_x0 / f_x0prime
+         #Calculate the next x
+         x_n1 = x0 - (func_val / derivative_calc)
 
         #check if the difference between x_n1 and x0 is tiny
-        if abs(x_n1 - x) < 1e-9:
-            then break 
-       #update  current x to be the new x_new for the next round
-        
+         if abs(x_n1 - x0) < 1e-9:
+             return x_n1
+       
+        #update  current x to be the new x_new for the next round
+        x0 = x_n1
 
 
     
-        
     
     
     
