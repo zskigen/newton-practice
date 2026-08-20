31abfd54d1fea41b5a30814b2f16b79673dda5d1
zskigen <z_skigen@berkeley.edu>
2026-08-20 09:51:07 -0700

    chris code

--- newton.py
+++ newton.py
@@ -1,33 +1,16 @@
-def derivative(f, x, h=1e-5):
-    """
-    compute derivative
-    """
-    return (f(x + h) - f(x - h)) / (2 * h)
+def deriv(f, x, eps = 1e-5):
+    return (f(x+eps) - f(x)) / eps
 
 
-def function_value(function, value):
-    """
-    compute function at a value
-    """
-    return function(value)
+def deriv2(f, x, eps = 1e-5):
+    return (deriv(f, x+eps, eps) - deriv(f, x, eps)) / eps
 
 
-def newtons_method(x0, function):
-    """
-    calculate function value and derivative value at current x, check if derivative is close to 0
-    then calculate the next x, and check if the difference between the new x and old x is small, and
-    then update the old x with the new x and repeat
-    """
-    while True:
-        func_val = function_value(function, x0)
-        derivative_calc = derivative(function, x0)
-
-        if abs(derivative_calc) < 1e-9:
-            print("too close to 0")
-            break
-
-        x_n1 = x0 - (func_val / derivative_calc)
-
-        if abs(x_n1 - x0) < 1e-9:
-            return x_n1
-        x0 = x_n1
+def optimize(x0, f, tol =1e-4):
+    x_new = x0 - deriv(f, x0)/ deriv2(f, x0)
+    x = x0
+    while abs(x_new - x) > tol:
+        x = x_new
+        x_new = x0 - deriv(f, x) / deriv2(f, x)
+    return {"x": x_new,
+            'value': f(x_new)}
